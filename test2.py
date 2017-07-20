b8e0cfb92c5b776288cf39bf65d93c38715fd073
cinna-zju <569095288@qq.com>
2017-07-20 14:35:56 -0400

    new plot

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from matplotlib import pyplot as plt
-
+import sqlite3
 
 label, alldata, loss = ld.get_data(15)
 
@@ -71,3 +71,44 @@ clf = tr.train(label, data)
 # plt.plot(alldata[0:17, 0], res[:17])
 # plt.show()
 
+conn = sqlite3.connect('./data/2')
+c = conn.cursor
+subdata = []
+for no in range(1,5):
+    sql = 'select * from emotions2_'+str(no)
+    c.execute(sql)
+    subdata.append(c.fetchall())
+    subdata[no-1] = np.array(subdata[no-1], dtype=np.float32)
+
+size = min(subdata[0].shape[0],
+    subdata[1].shape[0],
+    subdata[2].shape[0],
+    subdata[3].shape[0]
+)
+temp = subdata[0][0:size, :]
+for j in range(1, 4):
+    temp = np.column_stack((temp, subdata[j][0:size, :]))
+
+for j in range(0,4):
+    plt.subplot(5,1,i+1)
+    plt.plot(temp[:,0], temp[:, 2+11*j],
+        temp[:,0], temp[:, 3+11*j],
+        temp[:,0], temp[:, 4+11*j],
+        temp[:,0], temp[:, 5+11*j],
+        temp[:,0], temp[:, 6+11*j],
+        temp[:,0], temp[:, 7+11*j],
+        temp[:,0], temp[:, 8+11*j]
+    )
+
+EMOMASK = [0,0,1,1,1,1,1,1,1,0,0,
+    0,0,1,1,1,1,1,1,1,0,0,
+    0,0,1,1,1,1,1,1,1,0,0,
+    0,0,1,1,1,1,1,1,1,0,0
+]
+test = temp[:, EMOMASK]
+res = []
+for j in range(0, test.shape[0]):
+    res.append(clf.predict(test[i,:].reshape(1, -1)))
+
+plt.subplot(5,1,5)
+plt.plot(temp[:,0], res)
